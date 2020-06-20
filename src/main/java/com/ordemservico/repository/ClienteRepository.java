427c19210e62befa3fc942e5582241ff6d830bad
yuri alves <yurileader@gmail.com>
2020-06-20 18:51:47 -0300

    Criando o projeto e conectando ao banco de dados v01

---
+++ src/main/java/com/ordemservico/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.ordemservico.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ordemservico.domain.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
---
+++ src/test/java/com/ordemservico/OrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordemservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
