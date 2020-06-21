d8549d56df972d70368fcd4566221736e352d228
yuri alves <yurileader@gmail.com>
2020-06-21 13:44:57 -0300

    implementando metodo Put

--- src/main/java/com/ordemservico/controller/ClienteController.java
+++ src/main/java/com/ordemservico/controller/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +57,15 @@ public class ClienteController {
 		publisher.publishEvent(new HeaderLocationEvent(this, response, clienteSalvo.getId()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
+	}
+	
+	@PutMapping("/{id}")
+	public Cliente atualizar (@PathVariable Long id, @Valid @RequestBody Cliente cliente) {
+		
+		Cliente clienteAtualizar = clienteService.atualizar(cliente, id);
+		
+		return this.clienteRepository.save(clienteAtualizar);
+		
 	}
 	
 	@DeleteMapping("/{id}")
--- src/main/java/com/ordemservico/domain/service/CadastroClienteService.java
+++ src/main/java/com/ordemservico/domain/service/CadastroClienteService.java
@@ -1,6 +1,8 @@
 package com.ordemservico.domain.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ordemservico.domain.model.Cliente;
@@ -18,5 +20,24 @@ public class CadastroClienteService {
 	
 	public void excluir (Long id) {
 		clienteRepository.deleteById(id);
+	}
+	
+	public Cliente atualizar(Cliente cliente, Long id) {
+		
+		Cliente clienteSalvo = encontrarCliente(id);
+		/**BeansUtils pode ser usado para ajudar a tratar os dados para atualziar
+		 * Source: A fonte dos dados - no caso da classe pessoas
+		 * target: Para onde irei mandar os dados - no caso para minha variavel pessoaSalva
+		 * ignoreProperties: qual dado devo ignorar - no caso o codigo que é codigo*/
+		//BeanUtils.copyProperties(source, target, ignoreProperties);
+		BeanUtils.copyProperties(cliente, clienteSalvo, "id");
+		
+		return clienteRepository.save(clienteSalvo);
+	}
+	
+	public Cliente encontrarCliente(Long id) {
+		Cliente clienteSalvo = this.clienteRepository.findById(id)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return clienteSalvo;
 	}
 }
